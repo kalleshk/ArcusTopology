04d6f9ecf5f6dede866a353330bc3cceefeee8d5
kalleshk <kallesh.kavyabhi@gmail.com>
2024-10-24 01:34:31 +0530

    Update test_fix_startup_teardown_userdetails.py

--- HemaProjects/python_pytestProject/pytest_Concept_Files/tests/test_fix_startup_teardown_userdetails.py
+++ HemaProjects/python_pytestProject/pytest_Concept_Files/tests/test_fix_startup_teardown_userdetails.py
@@ -1,19 +1,18 @@
 import pytest
 
 #Define fixture
-
 @pytest.fixture
+
 #define setupcode
 def setup_teardown():
     #setup code
     print("\n Setup the user creditional")
     user_details = {"username":"Jhonson", "password":"Jhonson123", "email":"Jhonson@gmail.com"}
     yield user_details
-
+    
     #Teardown phase
     print("\n Clearing or rest the  user data")
-
-
+    
 #test user details
 def test_Userdetails(setup_teardown):
     user_data = setup_teardown
